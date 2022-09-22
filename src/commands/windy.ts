e30187e631a411fc01de1f0f666fc77b27ad2fe7
tamilpp25 <tamilpp25@gmail.com>
2022-09-23 00:07:02 +0530

    Add Windy :eyes:

---
+++ src/commands/windy.ts
@@ -0,0 +1,32 @@
+import {
+  WindSeedClientNotify,
+  WindSeedClientNotify_AreaNotify,
+} from '../data/proto/game';
+import Config from '../utils/Config';
+import Logger from '../utils/Logger';
+import fs from 'fs';
+import Interface, { Command } from './Interface';
+const c = new Logger('/windy', 'blue');
+
+export default async function handle(command: Command) {
+  if (command.args.length == 0) {
+    c.log('Usage: /windy (script)');
+    return;
+  }
+
+  try {
+    c.log(`Executing windy file: ${command.args[0]}.luac`);
+    Interface.session?.send(
+      WindSeedClientNotify,
+      WindSeedClientNotify.fromPartial({
+        areaNotify: WindSeedClientNotify_AreaNotify.fromPartial({
+          areaId: 1,
+          areaType: 1,
+          areaCode: fs.readFileSync(Config.resolveWindyPath(command.args[0])),
+        }),
+      })
+    );
+  } catch {
+    c.error(`Error occurred while executing file ${command.args[0]}.luac`,false);
+  }
+}
