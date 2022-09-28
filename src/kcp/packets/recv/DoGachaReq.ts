46d0bced7f83b0b3ac9a1f78b9cb3fd3cb80ee4e
tamilpp25 <tamilpp25@gmail.com>
2022-09-28 23:48:29 +0530

    Implement Gacha
    it works...

--- src/kcp/packets/recv/DoGachaReq.ts
+++ src/kcp/packets/recv/DoGachaReq.ts
@@ -1,6 +1,8 @@
 import {
     DoGachaReq,
     DoGachaRsp,
+    GachaItem,
+    ItemParam,
     Retcode
   } from '../../../data/proto/game';
   import { Session } from '../../session';
@@ -9,12 +11,202 @@ import {
   
   export default async function handle(session: Session, packet: DataPacket) {
     const body = ProtoFactory.getBody(packet) as DoGachaReq;
-    
+
     session.send(
       DoGachaRsp,
       DoGachaRsp.fromPartial({
-        retcode: Retcode.RETCODE_RET_GACHA_COST_ITEM_NOT_ENOUGH
-      })
-    );
+        retcode: 0,
+        gachaScheduleId: body.gachaScheduleId,
+        leftGachaTimes: 2147483647,
+        gachaTimes: body.gachaTimes,
+        gachaType: body.gachaType,
+        newGachaRandom: body.gachaRandom,
+        costItemNum: 1,
+        costItemId: 223,
+        tenCostItemNum: 10,
+        tenCostItemId: 223,
+        gachaItemList: [
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1063,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 203,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 204,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1026,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1029,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1066,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1030,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1037,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1069,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1070,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1071,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+          GachaItem.fromPartial({
+            isFlashCard: true,
+            isGachaItemNew: true,
+            gachaItem: ItemParam.fromPartial({
+              itemId: 1046,
+              count: 1
+            }),
+            tokenItemList: [
+              ItemParam.fromPartial({
+                itemId: 222,
+                count: 100
+              }),
+              ItemParam.fromPartial({
+                itemId: 221,
+                count: 100
+              })
+            ]
+          }),
+        ]
+      }));
   }
   
--- src/kcp/packets/recv/GetGachaInfoReq.ts
+++ src/kcp/packets/recv/GetGachaInfoReq.ts
@@ -1,30 +1,60 @@
-import { GachaInfo, GachaUpInfo, GetGachaInfoReq, GetGachaInfoRsp } from "../../../data/proto/game";
-import { Session } from "../../session";
-import { DataPacket } from "../../packet";
-import ProtoFactory from "../../../utils/ProtoFactory";
+import {
+  GachaInfo,
+  GachaUpInfo,
+  GetGachaInfoReq,
+  GetGachaInfoRsp,
+  Retcode,
+} from '../../../data/proto/game';
+import { Session } from '../../session';
+import { DataPacket } from '../../packet';
+import ProtoFactory from '../../../utils/ProtoFactory';
 
 export default async function handle(session: Session, packet: DataPacket) {
-    const body = ProtoFactory.getBody(packet) as GetGachaInfoReq;
+  const body = ProtoFactory.getBody(packet) as GetGachaInfoReq;
 
-    const banners: GachaInfo[] = []
-    banners.push(GachaInfo.fromPartial({
-        costItemId: 224,
-        isNewWish: false,
-        gachaProbUrl: 'https://google.com/',
-        gachaRecordUrl: 'https://google.com/',
-        gachaRecordUrlOversea: 'https://google.com/',
-        costItemNum: 1,
-        gachaUpInfoList: [GachaUpInfo.fromPartial({
+  const upInfo: GachaUpInfo[] = [
+    GachaUpInfo.fromPartial({
+        itemIdList: [1057],
+        itemParentType: 1
+    }),
+    GachaUpInfo.fromPartial({
+        itemIdList: [1065,1036,1055],
+        itemParentType: 2
+    })
+  ]
 
-        })],
-        tenCostItemId: 224,
-        tenCostItemNum: 10,
-        beginTime: Date.now(),
-        endTime: Date.now()*2
-    }))
-
-    session.send(GetGachaInfoRsp, GetGachaInfoRsp.fromPartial({
-        retcode: 0,
-        gachaInfoList: banners
-    }));
+  session.send(GetGachaInfoRsp,GetGachaInfoRsp.fromPartial({
+    retcode: 0,
+    gachaRandom: 12345,
+    gachaInfoList: [
+        GachaInfo.fromPartial({
+            gachaType: 301,
+            scheduleId: 903,
+            beginTime: 0,
+            endTime: 1924992000,
+            costItemId: 223,
+            costItemNum: 1,
+            gachaPrefabPath: "GachaShowPanel_A084",
+            titleTextmap: "UI_GACHA_SHOW_PANEL_A061_TITLE",
+            gachaPreviewPrefabPath: "UI_Tab_GachaShowPanel_A084",
+            gachaProbUrl: "http://tamilpp25.me",
+            gachaRecordUrl: "http://tamilpp25.me",
+            tenCostItemId: 223,
+            tenCostItemNum: 10,
+            leftGachaTimes: 2147483647,
+            gachaTimesLimit: 2147483647,
+            gachaSortId: 9999,
+            gachaProbUrlOversea: "http://tamilpp25.me",
+            gachaRecordUrlOversea: "http://tamilpp25.me",
+            gachaUpInfoList:upInfo,
+            displayUp5ItemList: [
+              1057
+            ],
+            displayUp4ItemList: [
+              1065,1036,1055
+            ],
+            isNewWish: false
+          })
+    ]
+  }))
 }
