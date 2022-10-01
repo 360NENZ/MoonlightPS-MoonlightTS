4ba62166405f560bd984be30b60ee755612c6d30
yarik0chka <yarik0chka@gmail.com>
2022-10-01 18:00:55 +0500

    Fix map issue

--- src/kcp/packets/recv/GetSceneAreaReq.ts
+++ src/kcp/packets/recv/GetSceneAreaReq.ts
@@ -23,7 +23,7 @@ export default async function handle(session: Session, packet: DataPacket) {
           level: 1,
         },
       ],
-      sceneId: 9,
+      sceneId: body.sceneId,
       areaIdList: [
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
         21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
