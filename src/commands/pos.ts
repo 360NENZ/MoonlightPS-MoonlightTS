6e6aa7b774736c1b4717507aca3f18a1cd5e6afb
tamilpp25 <tamilpp25@gmail.com>
2022-11-26 13:15:48 +0530

    player pos command

---
+++ src/commands/pos.ts
@@ -0,0 +1,9 @@
+import Logger from '../utils/Logger';
+import Interface, { Command } from './Interface';
+const c = new Logger('/pos', 'blue');
+
+export default async function handle(command: Command, executor: boolean) {
+    const player = Interface.session?.getPlayer()
+    Interface.handleMessage(`Position: X:${Math.round(player!.position.x)}, Y:${Math.round(player!.position.y)}, Z:${Math.round(player!.position.z)}`)
+    
+}
